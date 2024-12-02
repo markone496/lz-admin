92b06ffa632bbfd858cd88a57bc47b960a1fa622
wang9 <wtc>
2024-12-02 17:34:04 +0800

    测试

--- src/LzAdminProvider.php
+++ src/LzAdminProvider.php
@@ -13,7 +13,10 @@ class LzAdminProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/config/admin.php' => config_path('admin.php'),
         ], 'config');
-
+        // 发布资源文件
+        $this->publishes([
+            __DIR__ . '/assets' => public_path('vendor/lz/admin/src/assets'),
+        ], 'assets');
         // 加载路由
         $this->loadRoutesFrom(__DIR__ . '/routes/admin.php');
 
